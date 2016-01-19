f954b62504245defd912fb211c2a1cb4b784cb10
Jonas Friedmann <j@frd.mn>
2016-01-19 11:27:07 +0100

    Add config file

---
+++ config.default.php
@@ -0,0 +1,9 @@
+<?php
+  /*
+   *  Database configuration
+   */
+
+  define('DB_USERNAME', 'crt');
+  define('DB_PASSWORD', '123456');
+  define('DB_HOST', 'localhost');
+  define('DB_NAME', 'crt');
