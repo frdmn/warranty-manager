2bad2e475af842979600733b5441e47f0d0f8ebb
Jonas Friedmann <j@frd.mn>
2016-01-19 13:37:49 +0100

    Move DB check to API

--- api/index.php
+++ api/index.php
@@ -1,11 +1,32 @@
 <?php
+    // Auto load composer components
   require '../vendor/autoload.php';
 
-  $app = new \Slim\Slim();
+  // Require config
+  include('../config.php');
 
+  // Initalize Slim instance
+  $app = new \Slim\Slim();
   $app->view(new \JsonApiView());
   $app->add(new \JsonApiMiddleware());
 
+  // Check for necessarry credential/configuration constants
+  if (!defined('DB_HOSTNAME') || !defined('DB_NAME') | !defined('DB_USERNAME') | !defined('DB_PASSWORD')) {
+      die('{"msg":"Invalid database connection","error":true,"status":500}');
+  }
+
+  // Initalize database connection
+  $database = new medoo([
+      'database_type' => 'mysql',
+      'database_name' => constant('DB_NAME'),
+      'server' => constant('DB_HOSTNAME'),
+      'username' => constant('DB_USERNAME'),
+      'password' => constant('DB_PASSWORD'),
+      'charset' => 'utf8',
+      'prefix' => 'crt_',
+  ]);
+
+  // GET "/" route
   $app->get('/', function() use ($app) {
       $app->render(200,array(
               'msg' => 'Welcome to my json API!',
--- index.php
+++ index.php
@@ -6,22 +6,6 @@
 
     // Require config
     include('config.php');
-
-    // Check for necessarry credential/configuration constants
-    if (!defined('DB_HOSTNAME') || !defined('DB_NAME') | !defined('DB_USERNAME') | !defined('DB_PASSWORD')) {
-        die('Check config.php file and make sure to adjust your MySQL credentials.');
-    }
-
-    // Initalize database connection
-    $database = new medoo([
-        'database_type' => 'mysql',
-        'database_name' => constant('DB_NAME'),
-        'server' => constant('DB_HOSTNAME'),
-        'username' => constant('DB_USERNAME'),
-        'password' => constant('DB_PASSWORD'),
-        'charset' => 'utf8',
-        'prefix' => 'crt_',
-    ]);
 ?>
 <!DOCTYPE html>
 <html lang="en">
